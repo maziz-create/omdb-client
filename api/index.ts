28d3a7cc985f735c20aec0730e02d8d8f98a6ca5
Mehmet Aziz Algüllü <maacode@hotmail.com>
2022-03-18 18:30:41 +0300

    Deleted content-type

--- api/index.ts
+++ api/index.ts
@@ -2,7 +2,4 @@ import axios from "axios";
 
 export const httpClient = axios.create({
   baseURL: "http://www.omdbapi.com/?apikey=11b6d4ba&s=",
-  headers: {
-    "Content-Type": "application/json",
-  },
 });
