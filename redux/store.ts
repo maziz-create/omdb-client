f1d77fa690642605c5f6ed77b6b59fc90f6780d5
Mehmet Aziz Algüllü <maacode@hotmail.com>
2022-03-18 18:16:31 +0300

    Added redux store

---
+++ redux/store.ts
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import moviesSlice from "./movies/moviesSlice";
+
+export const store = configureStore({
+  reducer: {
+    movies: moviesSlice,
+  },
+});
