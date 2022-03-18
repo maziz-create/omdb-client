7c9f26bc4fb6d978033837c616bb35d39e653cc6
Mehmet Aziz Algüllü <maacode@hotmail.com>
2022-03-18 18:17:05 +0300

    Added movie model and redux provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
 import Head from "next/head";
 
-import "../styles/globals.css";
+import { Provider } from "react-redux";
+import { store } from "../redux/store";
+
 import { Fragment } from "react";
 import { AppProps } from "next/app";
+
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -10,7 +14,9 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <Component {...pageProps} />
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
     </Fragment>
   );
 }
