65b541e95d96ec2c2334c5c93cbfb2f53665b3d9
Mehmet Aziz Algüllü <maacode@hotmail.com>
2022-03-18 18:34:54 +0300

    Fix typo

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const httpClient = axios.create({
-  baseURL: "http://www.omdbapi.com/?apikey=11b6d4ba&s=",
+  baseURL: "https://www.omdbapi.com/?apikey=11b6d4ba&s=",
 });
